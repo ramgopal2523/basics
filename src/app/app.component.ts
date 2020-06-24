74891d619002f88cda01322b0cefc80231f8a384
varmaramgopal3 <varmaramgopal3@gmail.com>
2020-06-24 12:50:43 -0400

    interpolation concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  title = 'Project CC';
+
+  products = [{
+    name: 'Phone XL',
+    value: 2,
+    isDone: 3
+  },
+  {
+    name: 'Phone Mini',
+    value: 2,
+    isDone: 3
+  },
+  {
+    name: 'Phone Standard',
+    value: 2,
+    isDone: 3
+  }
+  ];
+
+  ngOnInit() {
+  }
 }
